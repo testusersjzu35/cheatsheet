403c3571ddf69614674453ed470e42ffe719a6b8
Pablo <ppmoya08@gmail.com>
2021-12-19 18:48:23 -0300

    Added binary file

--- remove_extension.py
+++ remove_extension.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#! /usr/bin/python3
 # -*- coding: utf-8 -*-
 
 """Remove_extension.py: Changes all extension name on all sibling directories ."""
@@ -9,6 +9,8 @@ __version__ = "1.0.1"
 
 import os
 import logging
+from colorama import Fore, init
+init()
 
 logging.basicConfig(format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p', filename='output.log', encoding='utf-8', level=logging.DEBUG)
 
@@ -16,48 +18,48 @@ logging.basicConfig(format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S
 def make_log(old, new):
     n = len(old)
     i = 0
-    
+
     changes_list = list()
-    while(i < n ):
+    for i in range(0, n):
         if old[i] != new[i]:
-            logging.info(f'INFO: Modified: {old[i]} -> {new[i]}')
-         
+            logging.info(f"INFO: Modified: { old[i] } -> { new[i] }")
+
             changes_list.append(f'.../{ "/".join( old[i].split("/")[-3::] )  } -> .../{ "/".join(new[i].split("/")[-3::] ) }')
         i += 1
-    
+
     return changes_list
 
 def change_extension(directories):
+    changes_list = list()
     for direc in directories:   
-        
+
         current_dir = os.listdir(direc)
         
+
         # check if directory is not empty
         if any(os.scandir(direc)):
             name_files_older = [ direc + '/' + x for x in current_dir ]
-            
+
             # List dir and get namefiles listdir(direc), if file ends with '.md' remove suffix '.md', if not dont do nothing
             file_name_list = [ x.removesuffix('.md') if x.endswith('.md') else x for x in current_dir ]
-            
+
             # Change 
             name_files_newer = [ direc + '/' + name  for name in file_name_list ]
-        else:
-            continue
         
-
-        try:
-            n = len(file_name_list)
             
+            n = len(file_name_list)
+
             for i in range(0,n):
                 if name_files_older[i] != name_files_newer[i]:
                     os.rename(name_files_older[i], name_files_newer[i])
-                    
-            return name_files_older, name_files_newer
-        
-        except FileNotFoundError as err:
-            logging.error(f'ERROR: {err}')
-            return
+
+            changes = make_log(name_files_older, name_files_newer)
+            changes_list.extend(changes)
             
+
+
+    return changes_list
+
 
 def get_list_directories():
     current_dir = os.getcwd()
@@ -66,25 +68,24 @@ def get_list_directories():
 
 
 def main():
-    try:
-        logging.info('INFO: Started')
-
-        directories = get_list_directories()
-        old_dir, new_dir = change_extension(directories)
-        
-        changes = make_log(old_dir, new_dir)
-
-
-        logging.info('INFO: Finished')
     
-    except Exception as e:
-        logging.error(f'ERROR: {e}')
-        print('Error Ocurred! Review the output.log file')
+    logging.info('INFO: Started')
 
-    print("Changes")
-    print( [mod for mod in changes][0] )
+    directories = get_list_directories()
+        
+    changes_list = change_extension(directories)
+
+        
+
+    logging.info('INFO: Finished')
+        
+    print(Fore.RED + "Changes".center(50, '-'))
+    
+   
+    
+    [ print(Fore.GREEN + changes) for changes in changes_list  ]
+
 
 if __name__ == '__main__':   
-    
+
     main()
-    
